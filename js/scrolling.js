fc73daa53ffe5bf91884d89aab76c15b010fe7bd
Viacheslav Litvinov <viacheslav.litvinov@students.stamford.edu>
2016-02-14 13:16:22 +0700

    PATIFY Prototype

---
+++ js/scrolling.js
@@ -0,0 +1,8 @@
+$(function(){
+	$("#buy-nav").on("click", function() {
+		$(window).scrollTo("#buy-section", {duration: 800});				
+	});
+	$("#sell-nav").on("click", function() {
+		$(window).scrollTo("#sell-section", {duration: 800});				
+	});
+});
